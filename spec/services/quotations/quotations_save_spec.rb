ecc29d0209fa2c7dbd01d2a83872db8b99082d5c
Sok444 <sok_88@mail.ru>
2018-08-13 18:53:37 +0300

    clean tests convert and save

--- spec/services/quotations/quotations_save_spec.rb
+++ spec/services/quotations/quotations_save_spec.rb
@@ -5,24 +5,8 @@ describe Quotations::QuotationsSave do
   let(:service) { described_class.new(quotation, params) } 
   
   let(:market) { create(:market) }
-  #let(:base_currency) { Currency.create(attributes_for(:currency)) }
-  #let(:quote_currency) do
-  #  Currency.create(attributes_for(:currency)) do |currency|
-  #    currency.short_name = 'ETH'
-  #    currency.full_name = 'Ethereum'
-  #  end
-  #end
-
-  #let(:currency_pair) { CurrencyPair.create(name: 'ETHBTC', base_currency_id: base_currency.id, quote_currency_id: quote_currency.id) }
   let(:currency_pair) { create(:currency_pair) }
 
-
-  #let(:time_interval) { TimeInterval.create(attributes_for(:time_interval)) }
-  #let(:period_quotation_type) do
-  #   PeriodQuotationType.create(attributes_for(:period_quotation_type)) do |period_quotation_type|
-  #    period_quotation_type.time_interval_id = time_interval.id
-  #  end
-  #end
   let(:period_quotation_type) { create(:period_quotation_type) }
 
   context 'Set valid quotation data.' do
